bb9dd3eae375d8b3d63ff8a5f5d803d73847f289
Daniel <k1403277@kingston.ac.uk>
2017-04-09 21:20:37 +0100

    Added static views for events, announcements and members

--- InformationApp/Controllers/HomeController.cs
+++ InformationApp/Controllers/HomeController.cs
@@ -1,16 +1,54 @@
-﻿using System;
+﻿using InformationApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace InformationApp.Controllers
 {
     public class HomeController : Controller
     {
+        private Members myMember = new Members
+        {
+            MemberId = 1,
+            Name = "Kevin",
+            EmailAddress = "Kevin@gmail.com",
+            Address = "45 KevinStreet 7NU 9NU",
+            PhoneNumber = 0293203212,
+            Location = "Kingston",
+            Biography = "Some cool book",
+            Skills = "Cooking"
+        };
+        private Events myEvent = new Events
+        {
+            EventId = 1,
+            EventName = "Swimming",
+            EventDesc = "Cool event",
+            EventLocation = "Kingston",
+            Date = DateTime.Now
+          
+        };
+        private Announcements myAnnouncement = new Announcements
+        {
+            AnnouncementId = 1,
+            AnnouncementName = "New cows!",
+            AnnouncementDescription = "There is new cows",
+            AnnouncementAuthor = "Kevin smith"
+          
+        };
         public ActionResult Index()
         {
-            return View();
+            return View(myMember);
+        }
+        public ActionResult Events ()
+        {
+            return View(myEvent);
+        }
+        public ActionResult Announcements ()
+        {
+            return View(myAnnouncement);
         }
 
         public ActionResult About()
